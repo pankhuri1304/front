f71b9732af6fa4675247edca4793bd5eddb02779
pankhuri1304 <113018538+pankhuri1304@users.noreply.github.com>
2023-04-22 14:06:41 +0530

    Update front.py

--- front.py
+++ front.py
@@ -7,7 +7,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.markdown('''<meta name="viewport" content="width=device-width, initial-scale=1.0">''', unsafe_allow_html=True)
+st.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">', unsafe_allow_html=True)
 
 
 
